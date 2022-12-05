c4959c2f543d10eabcfbe1cce90aec3395bc5a57
live7374 <5358207@gmail.com>
2022-12-05 14:30:43 +0200

    updated

--- HOMEWORK10/script.js
+++ HOMEWORK10/script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1adb8e42a8a28e73f33cf04842f23bcc2619b4cddb571eaa9d3bfd14bd76a17
-size 1596
+oid sha256:9190c9b8a4179b060be9bfd24225d75b3092bf420ffcbd7c7a63657219cbf38a
+size 1597
