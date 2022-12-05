09a73429a1b18240a4563a36b8e9ab86d918ee8d
live7374 <5358207@gmail.com>
2022-12-05 15:27:20 +0200

    updated

--- HOMEWORK10/script.js
+++ HOMEWORK10/script.js
@@ -1,3 +1,84 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9190c9b8a4179b060be9bfd24225d75b3092bf420ffcbd7c7a63657219cbf38a
-size 1597
+
+
+
+//Task 1
+
+let arr = ["Tom", "Sam", "Ray", "Bob"];
+let [x, y, , ...z] = arr;
+console.log(x); // "Tom"
+console.log(y); // "Sam"
+console.log(z); // [Bob]
+
+
+//Task 2
+
+let data = {
+    names: ["Sam", "Tom", "Ray", "Bob"],
+    ages: [20, 24, 22, 26],
+ };
+
+let {names:[name1, name2, name3, name4] ,ages:[age1, age2, age3, age4]} = data
+
+console.log(name2); // "Tom"
+console.log(age2); // 24
+console.log(name4); // "Bob"
+console.log(age4); // 26
+
+//Task 3
+function mul(...rest){
+let result = 1;
+let isNumber;
+for (let i of rest) {
+if (typeof i === 'number'){
+    result *= i;
+    isNumber = true;
+}}
+return isNumber ? result : 0;
+
+}
+console.log(mul(1, "str", 2, 3, true))
+console.log(mul(null, "str", false, true))
+
+//Task 4
+
+let server = {
+    data: 0,
+    convertToString: function (callback) {
+       callback(() =>{
+          return this.data + "";
+       });
+    }
+ };
+ let client = {
+    server: server,
+    result: "",
+    calc: function (data) {
+       this.server.data = data;
+       this.server.convertToString(this.notification());
+    },
+    notification: function () {
+       return (callback =>{
+          this.result = callback();
+       });
+    }
+ };
+ client.calc(123);
+ console.log(client.result); // "123"
+ console.log(typeof client.result); // "string"
+ 
+
+//Task 5
+
+function mapBuilder (keys, values) {
+    const map = new Map
+    for (let i = 0; i<keys.length; i++) {
+        map.set(keys[i], values [i]);
+    }
+    return map;
+}
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+console.log(map.size); 
+console.log(map.get(2)); 
--- HOMEWORK9/script.js
+++ HOMEWORK9/script.js
@@ -1,3 +1,49 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8354f489f12a4bb3d0f59ab3a77c3ec588b9613240b5c38e1ebe97118afd8f63
-size 826
+
+
+
+$(document).ready(function(){
+
+    //Task 1
+    $('a[href^="https://"]').attr('target', '_blank')
+    
+    
+    
+    //Task 2
+    $('.head').css ('background', 'green')
+    $('.inner').css ('font', '35px blue')
+    
+    
+    //$('h2.head').find('.inner).css()
+    
+    
+    
+    //Task 3
+    
+    
+    $('h3+div').each(function(){
+        let $divElems = $(this);
+        console.log(this);
+        $divElems.prev().before($divElems);
+    
+    })
+    
+    
+    // $('#t3').find('div').css('border', 'solid 2px blue');
+    
+    // $('#t3').find('div').prependTo('h3');
+    
+    // });
+    
+    
+    //Task 4
+    
+    const $checkbox = $(':checkbox');
+    $checkbox.on('click', function(){
+        if ($(':checkbox:checked').length == 3) {
+            $checkbox.attr('disabled', 'true');
+        }
+    });
+    
+    });
+    
+    
