480fd1062a198b4549df141380184c291552b7dc
live7374 <5358207@gmail.com>
2022-12-05 14:42:22 +0200

    HW updated

--- Project/script.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ae3ff96ae4e0de89b4bceea09756d2fa89ea6e857924f471b96c058e7bee3731
-size 2427
--- Project/tests/sample.test.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c9c092a433d1b04d3cc1962a821747f438ed622ebd9f7de0b9842c6f7a99b37c
-size 693
